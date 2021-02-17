e1c7e38a837b533a224eb0d39ae52a2a767672f8
student <rmrivero@miners.utep.edu>
2021-02-17 01:46:33 -0700

    Defined non_white_space functionin define.c. Tested in dtester for correct output.

--- src/define.c
+++ src/define.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include "tokenizer.h"
 
+int space_char(char c){
+  if(c == ' ' | c == '\n' | c == '\t'){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+int non_space_char(char c){
+  if(!space_char(c)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
--- src/dtester.c
+++ src/dtester.c
@@ -2,8 +2,9 @@
 #include "tokenizer.h"
 
 int main(){
-  char a = 'u';
-  printf("%d\n", space_char(a));
-
+  char a = ' ';
+  char b = ' ';
+  printf("space?: %d\n", space_char(a));
+  printf("non space?: %d\n", non_space_char(b));
   
 }
