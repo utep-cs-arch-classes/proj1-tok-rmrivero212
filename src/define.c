69ad9548e755a36ca98ebaba8143533b41e3b8d2
student <rmrivero@miners.utep.edu>
2021-02-21 01:01:32 -0700

    Finished word_start and word_end methods. Almost done working on count_words function.

--- src/define.c
+++ src/define.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "tokenizer.h"
 
+#define int LIMIT = 10;
+
 int space_char(char c){
   if(c == ' ' | c == '\n' | c == '\t'){
     return 1;
@@ -18,3 +20,39 @@ int non_space_char(char c){
     return 0;
   }
 }
+
+char *word_start(char *str){
+  
+  for(int i = 0; i < LIMIT && i != '\n'; i++){
+    if(non_space_char(*(str+i))){
+      return str+i;
+    }
+  }
+}
+
+char *word_end(char *str){
+
+  for(int i = 0; i < LIMIT && i != '\n'; i++){
+    if(space_char(*(str+i))){
+      return str+i;
+    }
+  }
+}
+
+int count_words(char *str){
+
+  
+  int counter = 0;
+  for(int i = 0; i < LIMIT && i != '\n'; i++){
+    if(word_end(str)){
+      counter++;
+    }
+    else if(*(word_end(str)+1))
+      
+    }
+
+  }
+}
+  
+}
+	
--- src/dtester.c
+++ src/dtester.c
@@ -2,9 +2,16 @@
 #include "tokenizer.h"
 
 int main(){
-  char a = ' ';
-  char b = ' ';
-  printf("space?: %d\n", space_char(a));
-  printf("non space?: %d\n", non_space_char(b));
+
+  char s[10] = "hi  guys";
+  char *str;
+  str = s;
+
+  printf("Address of beginning of word: %x\n", word_start(str));
+  printf("Contents of Address %x: %c\n", word_start(str), *word_start(str));
+  printf("Address of ending of word: %x\n", word_end(str));
+  
+  // printf("Address: %x\n", str);
+  // printf("Contents: %c\n", *(str+0));
   
 }
