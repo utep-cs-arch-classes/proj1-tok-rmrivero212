69a975df9c54bab6a33b33fdaa3f1e96daad4d26
student <rmrivero@miners.utep.edu>
2021-02-21 23:53:22 -0700

    Completed count_words method through extensive trial and error. Unable to complete readMe before deadline.

--- src/define.c
+++ src/define.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "tokenizer.h"
 
-#define int LIMIT = 10;
-
 int space_char(char c){
   if(c == ' ' | c == '\n' | c == '\t'){
     return 1;
@@ -22,8 +20,9 @@ int non_space_char(char c){
 }
 
 char *word_start(char *str){
-  
-  for(int i = 0; i < LIMIT && i != '\n'; i++){
+
+int LIMIT = 10;
+  for(int i = 0; i < LIMIT; i++){
     if(non_space_char(*(str+i))){
       return str+i;
     }
@@ -32,7 +31,8 @@ char *word_start(char *str){
 
 char *word_end(char *str){
 
-  for(int i = 0; i < LIMIT && i != '\n'; i++){
+int LIMIT = 10;
+  for(int i = 0; i < LIMIT; i++){
     if(space_char(*(str+i))){
       return str+i;
     }
@@ -40,19 +40,13 @@ char *word_end(char *str){
 }
 
 int count_words(char *str){
-
-  
+  int LIMIT = 10;
   int counter = 0;
-  for(int i = 0; i < LIMIT && i != '\n'; i++){
-    if(word_end(str)){
-      counter++;
-    }
-    else if(*(word_end(str)+1))
-      
-    }
-
+  char *i;
+  i = str;
+  while(*word_end(i) != '\n'){
+    counter++;
+    i = word_end(word_start(i));
   }
+  return counter;
 }
-  
-}
-	
--- src/dtester.c
+++ src/dtester.c
@@ -3,15 +3,15 @@
 
 int main(){
 
-  char s[10] = "hi  guys";
+  char s[10] = "hi we are\n";
   char *str;
   str = s;
 
   printf("Address of beginning of word: %x\n", word_start(str));
   printf("Contents of Address %x: %c\n", word_start(str), *word_start(str));
-  printf("Address of ending of word: %x\n", word_end(str));
+  printf("Address of ending of word: %x\n", word_end(word_start(str)));
+
+   printf("Count words: %d\n", count_words(str));
   
-  // printf("Address: %x\n", str);
-  // printf("Contents: %c\n", *(str+0));
   
 }
