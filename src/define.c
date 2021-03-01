4f04a3eedbdc8ae5baf2d2996ce6d08c145d0c5f
student <rmrivero@miners.utep.edu>
2021-02-28 23:51:56 -0700

    Completed print_tokens and free_tokens however had trouble getting the tokenize function to affectively traverse through the string. Hoping to have solution cleared up this week. Unable to complete tokenize before deadline.

--- src/define.c
+++ src/define.c
@@ -73,8 +73,8 @@ int len(char *start, char *end){
 }
 
 char **tokenize(char* str){
- 
-  char **arr = (char**)malloc(sizeof(char*) * (count_words(str) + 1));
+  char **arr;
+  arr = (char**)malloc(sizeof(char*) * (count_words(str) + 1));
   char wordcount = count_words(str);
   int l;
   int i = 0;
@@ -84,28 +84,29 @@ char **tokenize(char* str){
   
     while(i < wordcount){
       l = len( word_start(x), word_end(x));
-      s = copy_str(word_start(x), l + 1);
+      s = copy_str(x, l + 1);
       arr[i] = s; 
-      x = word_start(word_end(x));
       i++;
+      x = word_end(word_start(x));
     }
-    /*  
-  i = len(word_start(x), word_end(x));
-  s = copy_str(x, i + 1);
-  arr[0] = s;
-*/
   
-    arr[wordcount + 1] = '\0';
+    arr[wordcount + 1] = 0;
 
     return arr;
 }
 
 void print_tokens(char **tokens){
   int i = 0;
-  while(**(tokens+i)){
+  while(**(tokens+i) != 0){
     printf("%s\n", *tokens+i);
     i++;
   }
 }
 
-
+void free_tokens(char **tokens){
+  int i = 0;
+  while(**(tokens+i)){
+    free(tokens);
+    i++;
+  }
+}
--- src/dtester.c
+++ src/dtester.c
@@ -6,20 +6,14 @@ int len(char *start, char *end);
 int main(){
 
   char s[100] = "hey man\n";
-  char l[100] = "Hey man how is it going?";
   char *str;
-  char *sl;
   str = s;
-  sl = l;
-  /*
-  printf("Address of beginning of word: %x\n", word_start(str));
-  printf("Contents of Address %x: %c\n", word_start(str), *word_start(str));
-  printf("Address of ending of word: %x\n", word_end(word_start(str)));
-  */
+  
    printf("Count words: %d\n", count_words(str));
   
   printf("len: %d\n", len(word_start(str), word_end(str)));
   printf("copy_str: %s\n", copy_str(str, 3+1));
 
-  print_tokens(tokenize(str));
+ char **arr = tokenize(str);
+  print_tokens(arr);
 }
